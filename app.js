2a368ae7913a32c410433fff9fcbfacab2fa8b54
Dilou89 <odil_cadiou@orange.fr>
2017-01-08 22:55:24 +0100

    vzers1

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-	//votre code
+	//accrementer le compteur à chaque click sur le cookies
+	var counter = $(".counter");
+	for (var i = counter.length; i >= 0; i--) {
+		counter[i]
+	$("#qq.jpg").click(function(){
+		 
+	$('span').add(counter[i]);
+
+	})
+	
+	// donner le nombre total de click de la div
 
 
+};
 });
+
+
